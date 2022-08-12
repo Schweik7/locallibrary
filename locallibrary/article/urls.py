c4d339a863200e9ab587336ba713a1c2c772f111
Schweik7 <weibidewo0@gmail.com>
2022-08-12 17:43:57 +0800

    3rd

--- locallibrary/article/urls.py
+++ locallibrary/article/urls.py
@@ -7,4 +7,6 @@ app_name = 'article'
 urlpatterns = [
     # path('', RedirectView.as_view(url='/article/article-list/')),
     path('article-list/', views.article_list, name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('article-create/', views.article_create, name='article_create'),
 ]
--- locallibrary/article/views.py
+++ locallibrary/article/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render
 
 # 导入数据模型ArticlePost
 from .models import ArticlePost
+import markdown
+from django.shortcuts import render, redirect
+# 引入HttpResponse
+from django.http import HttpResponse
+# 引入刚才定义的ArticlePostForm表单类
+from .forms import ArticlePostForm
+# 引入User模型
+from django.contrib.auth.models import User
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body,
+        extensions=[
+        # 包含 缩写、表格等常用扩展
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+        ])
+    context = { 'article': article }
+    return render(request, 'article/detail.html', context)
 
 def article_list(request):
     # 取出所有博客文章
@@ -10,3 +31,20 @@ def article_list(request):
     context = { 'articles': articles }
     # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
+
+
+def article_create(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        article_post_form = ArticlePostForm()
+        context = { 'article_post_form': article_post_form }
+        return render(request, 'article/create.html', context)
